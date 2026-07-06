e01b353cacf3040ef9027078ee26a4020d79e098
FireDGun <tzachd1232@gmail.com>
2026-07-06 21:15:25 +0300

    add cors middleware and update package.json and package-lock.json

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import router from "./router.js"
-
+import cors from "cors"
 const app = express()
 
-
+app.use(cors({
+  origin: "http://127.0.0.1:5500",
+}))
 app.use(express.json())
 
 app.use(router)
